06a6927eb9973c3c164d59735dc1a5f4f183fc82
Abhibhardiya24 <abhishekbhardiya1995@gmail.com>
2023-08-31 18:32:14 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace HeightCategory
+{
+    public class Class1
+    {
+        class Program
+        {
+            static void Main()
+            {
+                int Height = 75;
+                double inch = 2.54;
+                double HeightinCM = Height * inch;
+                string result = null;
+
+                if (HeightinCM < 150)
+                {
+
+                    result = "Dwarf";
+                }
+
+                else if (HeightinCM > 150 && HeightinCM < 165)
+                {
+                    result = "Average Height";
+                }
+                else if (HeightinCM > 165 && HeightinCM < 195)
+                {
+
+                    result = "Tall";
+                }
+
+
+                else
+                {
+
+                    result = "Abnormal Height";
+                }
+
+
+
+                Console.WriteLine(result);
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
